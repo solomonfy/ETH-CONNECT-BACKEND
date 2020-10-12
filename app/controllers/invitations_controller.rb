80a08dec36ea99327f3fa1009aa9e78d717f9460
solomonfy <fantahunsolomon@gmail.com>
2020-10-12 10:33:48 -0400

    some change on invitation controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,17 +7,22 @@ class InvitationsController < ApplicationController
     def index
       @invitations = Invitation.all
 
-      render json: @invitations, except: [:created_at, :updated_at]
+      render json: @invitations, except: [:created_at, :updated_at], include: [:event => {include: [:host => {except: [:password_digest, :created_at, :updated_at]}]}]
     end
   
     # GET /invitations/1
     def show
-      render json: @invitation, except: [:created_at, :updated_at]
+      render json: @invitation, except: [:created_at, :updated_at], include: [:event => {include: [:host => {except: [:password_digest, :created_at, :updated_at]}]}]
+      # render json: @invitation, except: [:created_at, :updated_at]
     end
   
     # POST /invitations
     def create
-      @invitation = Invitation.new(invitation_params)
+      # @invitation = Invitation.new(invitation_params)
+      @invitation = Invitation.new(
+        attendee_id: params[:invitation][:attendee_id],
+        event_id: params[:invitation][:event_id]
+      )
   
       if @invitation.save
         render json: @invitation, status: :created, location: @invitation
