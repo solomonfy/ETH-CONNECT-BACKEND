42d1689102cd1c148355805ae6f3df205f4f3bb3
solomonfy <fantahunsolomon@gmail.com>
2021-05-10 16:05:27 -0400

    adding..

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :update, :destroy]∏
+  before_action :set_review, only: [:show, :update, :destroy]
   # skip_before_action :logged_in?, only: [:index, :show]
 
 
